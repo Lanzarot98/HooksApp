9d0514422f3ced48c188600ede2f60fc451fa547
lanzarot <luis-389@hotmail.com>
2022-09-09 19:09:46 -0500

    add and implement useLayoutEffect

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,12 +1,29 @@
-import { useFetch, useCounter } from "../hooks"
+import { useLayoutEffect, useRef, useState } from "react"
 
 
 export const Quote = ({author, quote}) => {
 
+    const pRef = useRef();
+    const [boxSize, setBoxSize] = useState({width: 0, height: 0}) // tamaño original de la caja
+
+    useLayoutEffect(() => {
+      // aquí puedo sacar el tamaño del contenedor
+        const {height, width} = pRef.current.getBoundingClientRect()
+        setBoxSize({ height, width })
+    }, [quote]) // podemos dejar esa dependencia en vació o con quote y esto podría dispararse cada vez que el quote cambie, aunque en el Loading esto se destruye y no es tan necesario
+
     return (
-        <blockquote className="blockquote text-end">
-            <p className="mb-1">{ quote }</p>
-            <footer className="blockquote-footer mt-1">{ author }</footer>
-        </blockquote>
+        <>
+
+            <blockquote 
+                className="blockquote text-end"
+                style={{display: 'flex'}}
+            >
+                <p ref={ pRef } className="mb-1">{ quote }</p>
+                <footer className="blockquote-footer mt-1">{ author }</footer>
+            </blockquote>
+
+            <code>{ JSON.stringify(boxSize) }</code>
+        </>
     )
 }
---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,45 @@
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "../03-examples"; // esto puedo hacerlo gracias al index.js de este mismo directorio
+
+
+export const Layout = () => {
+  
+  const { counter, increment, decrement} = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`);
+  
+  const {author, quote} = !!data && data[0];
+  // eso aparecerá apenas se cargue la página
+  // if(isLoading) {
+  //   return (
+  //     <h1>Loading...</h1>
+  //   )
+  // }
+  
+  
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {
+        (isLoading)
+          ? (<LoadingQuote />)
+          : ( <Quote author={ author } quote={ quote } />)
+      }
+
+      <button 
+        className="btn btn-primary"
+        disabled={ isLoading } // si loading es true se deshabilita
+        onClick={() => increment()}>
+        Next quote
+      </button>
+      <button 
+        className="btn btn-primary"
+        disabled={ isLoading } // si loading es true se deshabilita
+        onClick={() => decrement()}>
+        previous quote
+      </button>
+    </>
+  )
+}
