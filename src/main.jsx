3dfe6ff0c6f1e139082210745186c6968ed18e68
lanzarot <luis-389@hotmail.com>
2022-09-21 08:00:57 -0500

    conf react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 
-
-import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
+// import { TodoApp } from './08-useReducer/TodoApp';
 // import './08-useReducer/intro-reducer';
 // import { Padre } from './07-tarea-memo/Padre';
 // import { CallbackHook } from './06-memos/CallbackHook';
@@ -20,8 +21,11 @@ import { TodoApp } from './08-useReducer/TodoApp';
 
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  // </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
